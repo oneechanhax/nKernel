c2da9620eaaab6151854470a9e6c6bc5d846d903
oneechanhax <rebekahrowe9999@gmail.com>
2021-02-19 15:04:15 -0500

    Ooh, c++20 works muahahahahhaha

--- poc/src/entry.cpp
+++ poc/src/entry.cpp
@@ -1,7 +1,9 @@
 #include <stdbool.h>
-#include <stddef.h>
+//#include <stddef.h>
 #include <stdint.h>
- 
+
+#include <etl/string_view.h>
+
 /* Check if the compiler thinks you are targeting the wrong operating system. */
 #if defined(__linux__)
 #error "You are not using a cross-compiler, you will most certainly run into trouble"
@@ -32,18 +34,15 @@ enum vga_color {
 	VGA_COLOR_WHITE = 15,
 };
  
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) 
-{
+static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
 	return fg | bg << 4;
 }
  
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color) 
-{
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color) {
 	return (uint16_t) uc | (uint16_t) color << 8;
 }
  
-size_t strlen(const char* str) 
-{
+size_t strlen(const char* str) {
 	size_t len = 0;
 	while (str[len])
 		len++;
@@ -58,8 +57,7 @@ size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
  
-void terminal_initialize(void) 
-{
+void terminal_initialize() {
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
@@ -72,19 +70,16 @@ void terminal_initialize(void)
 	}
 }
  
-void terminal_setcolor(uint8_t color) 
-{
+void terminal_setcolor(uint8_t color) {
 	terminal_color = color;
 }
  
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
-{
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_entry(c, color);
 }
  
-void terminal_putchar(char c) 
-{
+void terminal_putchar(char c) {
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
@@ -93,22 +88,21 @@ void terminal_putchar(char c)
 	}
 }
  
-void terminal_write(const char* data, size_t size) 
-{
-	for (size_t i = 0; i < size; i++)
-		terminal_putchar(data[i]);
+void terminal_writestring(etl::string_view str) {
+    for (auto c : str)
+        terminal_putchar(c);
 }
- 
-void terminal_writestring(const char* data) 
-{
-	terminal_write(data, strlen(data));
-}
- 
+
+extern "C" { 
+
 void kernel_main(void) 
 {
 	/* Initialize terminal interface */
 	terminal_initialize();
  
 	/* Newline support is left as an exercise. */
-	terminal_writestring("Hello, kernel World!\n");
+    etl::string_view cpp_17_sv = "Konichiwa bishes";
+	terminal_writestring(cpp_17_sv);
+}
+
 }
---
+++ poc/src/etl_profile.h
@@ -0,0 +1,7 @@
+
+#pragma once
+
+#define ETL_NO_STL 0
+
+#define ETL_NULLPTR_INCLUDED 1
+#define ETL_NULLPTR nullptr
